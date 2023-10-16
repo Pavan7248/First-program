bf408930f10fbffaa93bf6adc66009bebb80c02a
Pavan7248 <144307541+Pavan7248@users.noreply.github.com>
2023-10-16 08:54:30 +0000

    Added escape sequence
    nothing

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 print("hello world")
 print(5)
 print("My name is Pavan")
+print(10+10)
 
+#Escape sequence
+print("Hello \"Pavan\"")
+print("Hello \'Pavan\'")
+print("Hello \nPavan")
+print("Hello \tPavan")
+print("Hello \rPavan")
